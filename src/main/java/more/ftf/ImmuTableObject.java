b3b3f45ca204513c32b337ff955ae48084f4ede6
fmbah <zhaoxin26@yahoo.com>
2019-04-14 23:20:16 +0800

    java 基础

--- src/main/java/more/ftf/ImmuTableObject.java
+++ src/main/java/more/ftf/ImmuTableObject.java
@@ -26,6 +26,10 @@ public final class ImmuTableObject {
         this.mp = mp;
     }
 
+    static class MyClass {
+        public final double i = Math.random();
+        public static double j = Math.random();
+    }
 
     public static void main(String[] args) {
         String text = "123";
@@ -36,5 +40,16 @@ public final class ImmuTableObject {
         text = "234";
         mp.put("444","444");
         System.out.println(immuTableObject.getText() + "=" + immuTableObject.getMp()+ "=>" + immuTableObject);
+
+
+        MyClass myClass1 = new MyClass();
+        MyClass myClass2 = new MyClass();
+        System.out.println(myClass1.i);
+        System.out.println(myClass1.j);
+        System.out.println(myClass2.i);
+        System.out.println(myClass2.j);
+
+        System.out.println(Math.round(-1.5));
+        System.out.println(Math.round(1.5));
     }
 }
